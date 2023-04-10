266e2d5bf8143c1f1895ff4a1c8697672b59dc9d
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2023-04-10 19:47:45 +0800

    Revamped layout of website, refactored JavaScript codes

---
+++ public/js/index.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+    var input = {
+        block_size: "",
+        set_size: "",
+        MM_memory_size: "",
+        MM_size_type: "",
+        cache_memory_size: "",
+        cache_size_type: "",
+        cache_cycle_time: "",
+        memory_cycle_time: "",
+        program_flow: "",
+        program_flow_type: ""
+    }
+
+    function getInput(obj){
+        obj.block_size = document.getElementById("block_size").value;
+        obj.set_size = document.getElementById("set_size").value;
+        obj.MM_memory_size = document.getElementById("MM_memory_size").value;
+        obj.MM_size_type = document.getElementById("MM_size_type").value;
+        obj.cache_memory_size = document.getElementById("cache_memory_size").value;
+        obj.cache_size_type = document.getElementById("cache_size_type").value;
+        obj.cache_cycle_time = document.getElementById("cache_cycle_time").value;
+        obj.memory_cycle_time =  document.getElementById("memory_cycle_time").value;
+        obj.program_flow = document.getElementById("program_flow").value;
+        obj.program_flow_type = document.getElementById("program_flow_type").value;
+    }
+
+    function resetError(){
+        document.getElementById("error").innerHTML = "";
+    }
+
+    function checkIfEmpty(obj){
+        var keys = Object.keys(obj);
+        var ifEmpty = false;
+        keys.forEach((key, index) => {
+            if(obj[key] == ""){
+                ifEmpty = true;
+            }
+        });
+        return ifEmpty;
+    }
+
+    function resetInput(){
+        document.getElementById("block_size").value = "";
+        document.getElementById("set_size").value= "";
+        document.getElementById("MM_memory_size").value= "";
+        document.getElementById("cache_memory_size").value= "";
+        document.getElementById("cache_cycle_time").value= 1;
+        document.getElementById("memory_cycle_time").value= 10;
+        document.getElementById("program_flow").value= "";
+    }
+
+    
+    $("#submit").click(function()
+    {
+        resetError();
+        getInput(input);
+        var validInput = true;
+        
+        if (checkIfEmpty(input))
+        {
+            var validInput = false;
+            document.getElementById("error").innerHTML = "Missing inputs";
+        }
+
+        if (validInput == true)
+        {
+            //TODO: algorithm for block set associative - MRU
+        }
+    });
+
+    $("#reset").click(function()
+    {
+        resetInput();
+    });
+});
